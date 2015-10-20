f218f26a9df06e1b09f77be192ef50889b175a74
Angel Stefano Nuñez <angstnun@users.noreply.github.com>
2015-10-20 17:29:04 -0300

    + Se agrego definicion de la base de datos en mrSistema.sql

---
+++ db/mrSistema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE perfil
+(
+	perfil_id INT UNSIGNED AUTO_INCREMENT,
+	nombre VARCHAR(25) NOT NULL,
+    PRIMARY KEY(perfil_id)
+);
+
+CREATE TABLE usuario
+(
+	usuario_id INT UNSIGNED AUTO_INCREMENT,
+	perfil_id INT UNSIGNED NOT NULL,
+	nombre VARCHAR(25) NOT NULL,
+	apellido VARCHAR(25) NOT NULL,
+	usuario VARCHAR(25) NOT NULL,
+	contrasena VARCHAR(32) NOT NULL,
+    PRIMARY KEY(usuario_id),
+	FOREIGN KEY(perfil_id) REFERENCES perfil(perfil_id)
+);
+
+CREATE TABLE turno
+(
+	turno_id INT UNSIGNED AUTO_INCREMENT,
+	usuario_id INT UNSIGNED,
+	nombreCliente VARCHAR(25) NOT NULL,
+	apellidoCliente VARCHAR(25) NOT NULL,
+	emailCliente VARCHAR(254) NOT NULL,
+	horaIngreso DATETIME NOT NULL,
+	horaAtencion DATETIME,
+    PRIMARY KEY(turno_id),
+	FOREIGN KEY(usuario_id) REFERENCES usuario(usuario_id)
+);

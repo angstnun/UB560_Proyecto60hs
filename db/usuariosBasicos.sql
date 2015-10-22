c5c63ad1558255babe23520fc90c0975c78ef9c6
Angel Stefano Nuñez <angstnun@users.noreply.github.com>
2015-10-22 12:18:33 -0300

    + Se agrego script para añadir usuario Debug a la tabla usuario y los correspondientes perfiles a perfil.

---
+++ db/usuariosBasicos.sql
@@ -0,0 +1,2 @@
+INSERT INTO perfil(nombre) VALUES('admin'),('asesor'), ('jefe'), ('cliente');
+INSERT INTO usuario(perfil_id, nombre, apellido, usuario, contrasena) VALUES(1, '', '', 'admin', 'toor');
